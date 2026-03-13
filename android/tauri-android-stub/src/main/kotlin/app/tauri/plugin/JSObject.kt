7363f8f936caf9bccac3396c0a6c727f6ce6ebc1
Charles R. Portwood II <charlesportwoodii@erianna.com>
2026-03-12 23:46:40 -0500

    chore: update tests
    Signed-off-by: Charles R. Portwood II <charlesportwoodii@erianna.com>

--- android/tauri-android-stub/src/main/kotlin/app/tauri/plugin/JSObject.kt
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024 charlesportwoodii@erianna.com
-// SPDX-License-Identifier: BSD-3-Clause
-
-package app.tauri.plugin
-
-open class JSObject {
-    fun put(key: String, value: Any?): JSObject = this
-}
--- android/tauri-android-stub/src/main/kotlin/app/tauri/plugin/Plugin.kt
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024 charlesportwoodii@erianna.com
-// SPDX-License-Identifier: BSD-3-Clause
-
-package app.tauri.plugin
-
-// Activity typed as Any so this stub compiles as a plain JVM library
-// without requiring the Android SDK.
-abstract class Plugin(protected val activity: Any)
